5ccf358e4340a1eab6466eacb84291ec603d172a
3n3a <46775561+3n3a@users.noreply.github.com>
2020-04-05 15:17:55 +0200

    Update video_actions.py

--- package/video_actions.py
+++ package/video_actions.py
@@ -2,13 +2,6 @@ from notion.client import NotionClient
 from notion.block import VideoBlock
 from notion.block import SubheaderBlock
 
-# variables, only if used standalone
-# token_v2_notion = "0c4f2816d3b75a757135dc5fa50c75848db11423f2490cdb8f6dee0dd0e980632e03700654abbaa45e49e9a3de1cb83d8f6851cdd6f21e733ca42e9bb4919837f3efedca95aa70e399a330160717"
-#page_url_notion= "https://www.notion.so/sluz/testeeeee-de4cbc5544384383a5941957bcac6712"
-
-# client = NotionClient(token_v2=token_v2_notion)
-# page = client.get_block(page_url_notion)
-
 def create_subh(page, title):
     page.children.add_new(SubheaderBlock, child_list_key=None, title=title)
     print("added subheader block with title " + title)
